b117716e6f79eabb33d3ea5cf89c7b83031c0355
claudionono14 <claudio.nono14@gmail.com>
2021-04-22 11:18:30 -0300

    primeiro commit

---
+++ Exercícios-JS/Fundamentos/comentarios.js
@@ -0,0 +1,10 @@
+//Comentários são textos que explicam partes do código
+
+// -> Representa comentários de UMA ÚNICA LINHA.
+console.log('Linha 1');
+
+/* -> Estes são comentários 
+ de múltiplas linhas. */
+ console.log('Linha 1');
+
+ 
---
+++ Exercícios-JS/Fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+//JavaScript é organizado por SENTENÇAS e BLOCOS de código
+
+console.log('sentença de código'); //sentença de código
+
+{ //primeiro bloco de código
+    
+    { //segundo bloco de código
+        console.log('Olá');
+        console.log('Mundo');
+    }
+    
+}
