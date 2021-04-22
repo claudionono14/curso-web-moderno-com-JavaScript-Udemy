ee3603a066f9d5762982d3e6cef925ce5b8bc44a
claudionono14 <claudio.nono14@gmail.com>
2021-04-22 15:41:25 -0300

    outros arquivos

---
+++ Exercícios-JS/Fundamentos/tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer); //o TYPEOF informa o tipo de dado que essa variável recebe
+
+qualquer = 3.151;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evitar nome genérico e siglas
+let valor = 'eok';
+let numero;
---
+++ Exercícios-JS/Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,7 @@
+var a = 3; // Variável de escopo global
+
+let b = 'claudio'; //Variável de escopo que substitui o VAR. Este tipo de variável tem significancia de escopo.
+ 
+const c = 2+2; // Valor constante
+
+console.log(a, b, c);
