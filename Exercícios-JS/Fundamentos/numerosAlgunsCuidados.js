ab5da0f878580744270b3365110c4c5746207c23
claudionono14 <claudio.nono14@gmail.com>
2021-06-28 14:40:44 -0300

    adicionando exemplo de cuidados com números

--- Exercícios-JS/Fundamentos/numerosAlgunsCuidados.js
+++ Exercícios-JS/Fundamentos/numerosAlgunsCuidados.js
@@ -1,9 +1,19 @@
 console.log(7 / 0); // Infinity, é a resposta do JS para números divididos por ZERO
+
 console.log('10' / 2);/* 5, o JS interpreta o valor da string e, caso seja possível transformar em número, ele transforma
-e executa a operação matemática*/
+e executa a operação matemática. A exceção é quando o operador matemático é a ADIÇÃO. Abaixo temos o exemplo.*/
+
+/* ATENÇÃO REDOBRADA !!! */
+console.log('10' + 2); // 102, o JS dá preferência à concatenação da STRING quando se usa o sinal de adição.
+
+console.log('10' - 2);/* 8, o JS, assim como no exemplo da divisão, vai transformar a STRING em número e executar
+normalmente a operação matemática*/
+
 console.log('Show' * 2); // NaN, o JS não vai repetir a palavra, como acontece em outras linguagens. 
+
 console.log(0.1 + 0.7); /* 0.7999999999999999, A especificaçãço do JS não suporta a precisão exata de contas com números de ponto flutuante
 logo, a resposta é bem aproximada, porém não precisa.*/
 /* console.log(10.toString()); => Vai gerar um erro pois o JS não executa a função direto no número. Para fazer isso
 é necessário usar o número entre parênteses, conforme o exemplo abaixo.*/
+
 console.log((10).toString()); // 10, executa normalmente a função 'toString'.
---
+++ Exercícios-JS/Fundamentos/strings.js
@@ -0,0 +1,8 @@
+/** STRINGS são cadeias de caracteres delimitadas por aspas simples, duplas ou crases (template string) */
+
+const escola = "Cod3r";
+
+console.log(escola.charAt(4)); // Retorna o caracter de índice 4 que, no caso, é a letra r.
+/** OU */
+console.log(escola[4]); // Retorna o  mesmo que o exemplo anterior.
+
